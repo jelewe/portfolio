f2a24a7a46ca386426e8f315b0ee2cb766f7873b
Jessie <hire.jesslw@gmail.com>
2023-04-30 10:36:48 -1000

    change project imgs to link for better accessibility, add styling for keywords in project paragraph

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -14,21 +14,21 @@ const Projects = () => {
             img: i1,
             title: 'eCommerce Site',
             link: 'https://jelewe.github.io/react-shopping-cart/',
-            notes: 'Web Design concept for a furniture store. Developed using functional React.js components, hooks, and Bootstrap. A third party API was implemented to generate product list. Design focus was accessibility and efficiency.'
+            notes: <span>Web Design concept for a furniture store. Developed using functional <code>React.js</code> components, hooks, <code>Bootstrap</code>, <code>HTML</code>, and <code>CSS</code>. A third party API was implemented to generate product list. Design focus was accessibility and efficiency.</span>
         },
         {
             id: 2,
             img: i2,
             title: 'Weather Now',
             link: 'https://jelewe.github.io/weather-app/',
-            notes: 'Web App developed using vanilla JavaScript, HTML, and CSS. Open Weather API was implemented to fetch real-time data based on user input. Design focus was accessibility and simplicity.'
+            notes: <span>Web App developed using vanilla <code>JavaScript</code>, <code>HTML</code>, and <code>CSS</code>. Open Weather API was implemented to fetch real-time data based on user input. Design focus was accessibility and simplicity.</span>
         },
         {
             id: 3,
             img: i3,
             title: 'To Do Tracker',
             link: 'https://jelewe.github.io/todo-app/',
-            notes: 'Web App developed using vanilla JavaScript, HTML, and CSS. Implemented local storage for task continuity across user sessions. Design focus was on accessibility and solidifying core JavaScript fundamentals.'
+            notes: <span>Web App developed using vanilla <code>JavaScript</code>, <code>HTML</code>, and <code>CSS</code>. Implemented local storage for task continuity across user sessions. Design focus was on accessibility and solidifying core JavaScript fundamentals.</span>
         }
     ]
 
@@ -58,9 +58,11 @@ const Projects = () => {
                                         ease: "easeInOut"
                                         } }
                                     >
-                    <img src= {proj.img} 
-                            alt= {proj.title}
-                            />
+                    <a href={proj.link}>
+                        <img src= {proj.img} 
+                                alt=  {`Link to ${proj.title}`}
+                                />
+                    </a>
                     <div className="projText">
                         <a href={proj.link}><h3>{proj.title}</h3></a>
                         <span>{proj.notes}</span>
